c96d046bf514ed843f9c358afc85a52b968dd8c3
Alexandra <alexandra.martin03@e-uvt.ro>
2024-05-27 16:12:10 +0300

    included_multi_threading_for_handling_encryption_decryption_simultaneously

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <random>
+#include <thread>
 #include "secure_crypt.cpp"
+
+void encrypt_message(const std::string &message, int key, std::string &encrypted_message) {
+    encrypted_message = encrypt(message, key);
+}
+
+void decrypt_message(const std::string &encrypted_message, int key, std::string &decrypted_message) {
+    decrypted_message = decrypt(encrypted_message, key);
+}
 
 int main() {
     std::string message = "Hello, SecureCrypt!";
@@ -9,9 +18,14 @@ int main() {
     std::uniform_int_distribution<> dis(1, 255);
     int key = dis(gen);
 
-    std::string encrypted_message = encrypt(message, key);
-    std::string decrypted_message = decrypt(encrypted_message, key);
+    std::string encrypted_message, decrypted_message;
 
+    std::thread encrypt_thread(encrypt_message, std::cref(message), key, std::ref(encrypted_message));
+    encrypt_thread.join();
+
+    std::thread decrypt_thread(decrypt_message, std::cref(encrypted_message), key, std::ref(decrypted_message));
+    decrypt_thread.join();
+    
     std::cout << "Original message: " << message << std::endl;
     std::cout << "Encryption key: " << key << std::endl;
     std::cout << "Encrypted message: " << encrypted_message << std::endl;
