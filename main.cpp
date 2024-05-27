75547f8215e610f2afcf01c547a0c7adaefe386e
Alexandra <alexandra.martin03@e-uvt.ro>
2024-05-27 16:08:25 +0300

    included_number_randomiser_for_encryption_key

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
+#include <random>
 #include "secure_crypt.cpp"
 
 int main() {
     std::string message = "Hello, SecureCrypt!";
-    int key = 3;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 255);
+    int key = dis(gen);
 
     std::string encrypted_message = encrypt(message, key);
     std::string decrypted_message = decrypt(encrypted_message, key);
 
     std::cout << "Original message: " << message << std::endl;
+    std::cout << "Encryption key: " << key << std::endl;
     std::cout << "Encrypted message: " << encrypted_message << std::endl;
     std::cout << "Decrypted message: " << decrypted_message << std::endl;
 
