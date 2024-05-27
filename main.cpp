5e234e6c3b99666f01c99dfd55463069a87db60d
Alexandra <alexandra.martin03@e-uvt.ro>
2024-05-27 15:41:50 +0300

    include_secure_crypt.cpp_and_encryption_decryption_testing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "secure_crypt.cpp"
 
 int main() {
-    
+    std::string message = "Hello, SecureCrypt!";
+    int key = 3;
+
+    std::string encrypted_message = encrypt(message, key);
+    std::string decrypted_message = decrypt(encrypted_message, key);
+
+    std::cout << "Original message: " << message << std::endl;
+    std::cout << "Encrypted message: " << encrypted_message << std::endl;
+    std::cout << "Decrypted message: " << decrypted_message << std::endl;
+
+    return 0;
 }
