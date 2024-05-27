e9d91dccc8d361ecd14e87761b2be5ebf6908790
Alexandra <alexandra.martin03@e-uvt.ro>
2024-05-27 16:19:47 +0300

    added_error_handling

--- secure_crypt.cpp
+++ secure_crypt.cpp
@@ -1,6 +1,9 @@
 #include <final_project.h>
 
 std::string encrypt(const std::string &message, int key) {
+    if (key <= 0) {
+        throw std::invalid_argument("Key must be positive");
+    }
     std::string encrypted_message = message;
     for (char &c : encrypted_message) {
         c +=key;
@@ -9,6 +12,9 @@ std::string encrypt(const std::string &message, int key) {
 }
 
 std::string decrypt(const std::string &encrypted_message, int key) {
+    if (key <= 0) {
+        throw std::invalid_argument("Key must be positive");
+    }
     std::string message = encrypted_message;
     for (char &c : message) {
         c -= key;
